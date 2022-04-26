3aec4112370dd54000c645a922a732e15a0df720
Edenilson Nascimento <67112084+Edenilson-Nascimento@users.noreply.github.com>
2022-04-25 22:41:34 -0300

    Update atividade_lista_pizzaria.sql

--- atividade_lista_pizzaria.sql
+++ atividade_lista_pizzaria.sql
@@ -1,3 +1,19 @@
+/*Crie um banco de dados para um serviço de uma Pizzaria. O nome do Banco de dados deverá ter o seguinte nome db_pizzaria_legal. O sistema trabalhará com as informações dos produtos comercializados pela empresa. O sistema trabalhará com 2 tabelas tb_pizzas e tb_categorias, que deverão estar relacionadas.
+
+Crie a tabela tb_categorias e determine pelo menos 2 atributos, além da Chave Primária, relevantes para classificar as pizzas.
+Crie a tabela tb_pizzas e determine 4 atributos, além da Chave Primária, relevantes aos produtos da pizzaria.
+Não esqueça de criar a Foreign Key da tabela tb_categorias na tabela tb_pizzas.
+Insira 5 registros na tabela tb_categorias.
+Insira 8 registros na tabela tb_pizzas, preenchendo a Chave Estrangeira para criar a relação com a tabela tb_categorias.
+Faça um SELECT que retorne todas as pizzas cujo valor seja maior do que R$ 45,00.
+Faça um SELECT que retorne todas as pizzas cujo valor esteja no intervalo R$ 50,00 e R$ 100,00.
+Faça um SELECT utilizando o operador LIKE, buscando todas as pizzas que possuam a letra M no atributo nome.
+Faça um SELECT utilizando a cláusula INNER JOIN, unindo os dados da tabela tb_pizzas com os dados da tabela tb_categorias.
+Faça um SELECT utilizando a cláusula INNER JOIN, unindo os dados da tabela tb_pizzas com os dados da tabela tb_categorias, onde traga apenas as pizzas que pertençam a uma categoria específica (Exemplo: Todas as pizzas que são doce).
+Salve todas as queries para cada um dos requisitos do exercício em um único script (arquivo .SQL) e coloque no seu Github pessoal, no repositório que você criou sobre Banco de dados.
+ 
+ */
+ 
  create database db_pizzaria_legal;
 
 use  db_pizzaria_legal;
