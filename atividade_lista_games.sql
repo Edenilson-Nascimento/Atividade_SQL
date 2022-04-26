655473e7b580ac5404723003a3d0b5ea79ec9acc
Edenilson Nascimento <67112084+Edenilson-Nascimento@users.noreply.github.com>
2022-04-25 22:38:48 -0300

    Update atividade_lista_games.sql

--- atividade_lista_games.sql
+++ atividade_lista_games.sql
@@ -1,3 +1,18 @@
+/*Crie um banco de dados para um serviço de um Games Online. O nome do Banco de dados deverá ter o seguinte nome db_generation_game_online. O sistema trabalhará com as informações dos personagens do jogo. O sistema trabalhará com 2 tabelas tb_personagens e tb_classes, que deverão estar relacionadas.
+
+    1. Crie a tabela tb_classes e determine pelo menos 2 atributos, além da Chave Primária, relevantes para classificar os personagens do Game Online.
+    2. Crie a tabela tb_personagens e determine 4 atributos, além da Chave Primária, relevantes aos personagens do Game Online.
+    3. Não esqueça de criar a Foreign Key da tabela tb_classes na tabela tb_personagens.
+    4. Insira 5 registros na tabela tb_classes.
+    5. Insira 8 registros na tabela tb_personagens, preenchendo a Chave Estrangeira para criar a relação com a tabela tb_classes.
+    6. Faça um SELECT que retorne todes os personagens cujo poder de ataque seja maior do que 2000.
+    7. Faça um SELECT que retorne todes os personagens cujo poder de defesa esteja no intervalo 1000 e 2000.
+    8. Faça um SELECT utilizando o operador LIKE, buscando todes os personagens que possuam a letra C no atributo nome.
+    9. Faça um SELECT utilizando a cláusula INNER JOIN, unindo os dados da tabela tb_personagens com os dados da tabela tb_classes.
+    10. Faça um SELECT utilizando a cláusula INNER JOIN, unindo os dados da tabela tb_personagens com os dados da tabela tb_classes, onde traga apenas os personagens que pertençam a uma classe específica (Exemplo: Todes os personagens da classe dos arqueiros).
+    11. Salve todas as queries para cada um dos requisitos do exercício em um único script (arquivo .SQL) e coloque no seu Github pessoal, no repositório que você criou sobre Banco de dados.
+*/
+
 create database  db_generation_game_online;
 
 use  db_generation_game_online;
